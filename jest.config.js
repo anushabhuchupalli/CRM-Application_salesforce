d4ef0d14659207e81de0a68b4082e96736c11de2
Anusha <116107942+anushabhuchupalli@users.noreply.github.com>
2024-10-27 10:45:21 +0530

    Delete jest.config.js

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-const { jestConfig } = require('@salesforce/sfdx-lwc-jest/config');
-
-module.exports = {
-    preset: '@lwc/jest-preset',
-    moduleFileExtensions: ['js', 'html'],
-    transformIgnorePatterns: [
-        'node_modules/(?!(lightning|@salesforce)/)',
-    ],
-};
